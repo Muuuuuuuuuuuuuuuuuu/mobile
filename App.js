034a8d9836ac74207d7479701b2d20e98e5bf60b
supattra <supattra4800@gmail.com>
2026-01-28 14:06:21 +0700

    lab 6

--- App.js
+++ App.js
@@ -1,44 +1,11 @@
-import React, { useState } from 'react';
-import RegistrationScreen from './src/screens/Registration';
-import ProfileScreen from './src/screens/Profile';
-import AboutScreen from './src/screens/About'; 
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import MainNavigator from './src/navigation/MainNavigator'; 
+
 export default function App() {
-  const [page, setPage] = useState('register');
-  const [form, setForm] = useState({
-    fullname: '',
-    email: '',
-    username: '',
-    password: '',
-    imageUrl: '',
-  });
-
-  const handleChange = (name, value) => {
-    setForm(prev => ({ ...prev, [name]: value }));
-  };
-
   return (
-    <>
-      {page === 'register' && (
-        <RegistrationScreen
-          form={form}
-          handleChange={handleChange}
-          onSubmit={() => setPage('profile')}
-          onNavigate={(target) => setPage(target)} 
-        />
-      )}
-
-      {page === 'profile' && (
-        <ProfileScreen 
-          form={form} 
-          onBack={() => setPage('register')} 
-        />
-      )}
-
-      {page === 'about' && (
-        <AboutScreen 
-          onBack={() => setPage('register')} 
-        />
-      )}
-    </>
+    <NavigationContainer>
+      <MainNavigator/>
+    </NavigationContainer>
   );
 }
